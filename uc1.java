43e097916a4662b02fc7774b82bf623080e3dd62
Abhilash21-hash <chandarajuabhilash21@gmail.com>
2026-04-12 15:26:38 +0530

    pushins of week 1 pratice questions

--- uc1.java
+++ uc1.java
@@ -1,4 +1,5 @@
 
+//level 1 of java programms//
 //1. Age of Harry//
 public class AgeOfHarry {
     public static void main(String[] args) {
@@ -133,5 +134,316 @@ public class HeightConversion {
         System.out.println("Your Height in cm is " + cm +
         " while in feet is " + feet +
         " and inches is " + remainingInches);
+    }
+}
+//Level 2 Practice Programs (question-wise in Java)://
+//1. Basic Calculator//
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+
+        double add = number1 + number2;
+        double sub = number1 - number2;
+        double mul = number1 * number2;
+        double div = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+        + number1 + " and " + number2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+    }
+}
+// 2. Area of Triangle (sq cm & sq inches)//
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double base = input.nextDouble();
+        double height = input.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaInch = areaCm / (2.54 * 2.54);
+
+        System.out.println("The Area of the triangle in sq in is " + areaInch +
+        " and sq cm is " + areaCm);
+    }
+}
+//3. Side of Square from Perimeter//
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double perimeter = input.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side +
+        " whose perimeter is " + perimeter);
+    }
+}
+//4. Distance Conversion (feet → yards & miles)//
+import java.util.Scanner;
+
+public class DistanceConvert {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double feet = input.nextDouble();
+
+        double yards = feet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("The distance in yards is " + yards +
+        " while the distance in miles is " + miles);
+    }
+}
+//5. Total Purchase Price//
+import java.util.Scanner;
+
+public class TotalPrice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double unitPrice = input.nextDouble();
+        int quantity = input.nextInt();
+
+        double total = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + total +
+        " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+    }
+}
+//6. Quotient and Remainder//
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.println("The Quotient is " + quotient +
+        " and Reminder is " + remainder +
+        " of two number " + number1 + " and " + number2);
+    }
+}
+//7. IntOperation Program//
+import java.util.Scanner;
+
+public class IntOperation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+        System.out.println("The results of Int Operations are " +
+        result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+    }
+}
+//8. DoubleOperation Program//
+import java.util.Scanner;
+
+public class DoubleOperation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+
+        System.out.println("The results of Double Operations are " +
+        result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+    }
+}
+//Level 3 Practice Programs
+//1. Celsius to Fahrenheit//
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double celsius = input.nextDouble();
+        double fahrenheitResult = (celsius * 9/5) + 32;
+
+        System.out.println("The " + celsius + " celsius is " + fahrenheitResult + " fahrenheit");
+    }
+}
+//2. Fahrenheit to Celsius//
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double fahrenheit = input.nextDouble();
+        double celsiusResult = (fahrenheit - 32) * 5/9;
+
+        System.out.println("The " + fahrenheit + " fahrenheit is " + celsiusResult + " celsius");
+    }
+}
+//3. Total Income (Salary + Bonus)//
+import java.util.Scanner;
+
+public class TotalIncome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double salary = input.nextDouble();
+        double bonus = input.nextDouble();
+
+        double income = salary + bonus;
+
+        System.out.println("The salary is INR " + salary +
+        " and bonus is INR " + bonus +
+        ". Hence Total Income is INR " + income);
+    }
+}
+//4. Travel Details Program//
+import java.util.Scanner;
+
+public class TravelDetails {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.next();
+        String fromCity = input.next();
+        String viaCity = input.next();
+        String toCity = input.next();
+
+        double fromToVia = input.nextDouble();
+        double viaToFinalCity = input.nextDouble();
+
+        double timeFromToVia = input.nextDouble();
+        double timeViaToFinalCity = input.nextDouble();
+
+        double totalDistance = fromToVia + viaToFinalCity;
+        double totalTime = timeFromToVia + timeViaToFinalCity;
+
+        System.out.println("The Total Distance travelled by " + name +
+        " from " + fromCity + " to " + toCity +
+        " via " + viaCity + " is " + totalDistance +
+        " km and the Total Time taken is " + totalTime + " minutes");
+    }
+}
+//5. Swap Two Numbers//
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        System.out.println("The swapped numbers are " + number1 + " and " + number2);
+    }
+}
+//6. Athlete Rounds in Triangular Park//
+import java.util.Scanner;
+
+public class AthleteRounds {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double side1 = input.nextDouble();
+        double side2 = input.nextDouble();
+        double side3 = input.nextDouble();
+
+        double perimeter = side1 + side2 + side3;
+        double rounds = 5000 / perimeter;
+
+        System.out.println("The total number of rounds the athlete will run is " + rounds + " to complete 5 km");
+    }
+}
+//7. Chocolates Distribution//
+import java.util.Scanner;
+
+public class Chocolates {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int chocolates = input.nextInt();
+        int children = input.nextInt();
+
+        int each = chocolates / children;
+        int remaining = chocolates % children;
+
+        System.out.println("The number of chocolates each child gets is " + each +
+        " and the number of remaining chocolates are " + remaining);
+    }
+}
+//8. Simple Interest//
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double principal = input.nextDouble();
+        double rate = input.nextDouble();
+        double time = input.nextDouble();
+
+        double SI = (principal * rate * time) / 100;
+
+        System.out.println("The Simple Interest is " + SI +
+        " for Principal " + principal +
+        ", Rate of Interest " + rate +
+        " and Time " + time);
+    }
+}
+//9. Pounds to Kilograms//
+import java.util.Scanner;
+
+public class WeightConvert {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double pounds = input.nextDouble();
+        double kg = pounds / 2.2;
+
+        System.out.println("The weight of the person in pounds is " + pounds +
+        " and in kg is " + kg);
+    }
+}
+//10. Maximum Handshakes//
+import java.util.Scanner;
+
+public class Handshakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int handshakes = (n * (n - 1)) / 2;
+
+        System.out.println("The maximum number of handshakes is " + handshakes);
     }
 }
